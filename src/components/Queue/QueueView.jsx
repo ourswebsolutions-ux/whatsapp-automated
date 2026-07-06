4d124069c0027b5ca3ad8379f857ce39ce8bde19
Ours Web Solutions <ourswebsolutions@gmail.com>
2026-07-06 12:02:47 +0500

    Sf

--- src/components/Queue/QueueView.jsx
+++ src/components/Queue/QueueView.jsx
@@ -50,7 +50,7 @@ console.log(phone,"this is the wahtsap phone")
 useEffect(() => {
   const fetchUser = async () => {
     try {
-     const res = await fetch(`https://https://outreach.axorawebsolutions.com/api/hello?phone=${phone}`, {
+     const res = await fetch(`//https://outreach.axorawebsolutions.com/api/hello?phone=${phone}`, {
   method: "GET",
 });
       const text = await res.text();
